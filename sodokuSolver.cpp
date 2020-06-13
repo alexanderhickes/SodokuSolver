748b220a2c9da3fae1d82cbc12cb97d49e516a34
alexanderhickes <alexhickes@yahoo.com>
2020-06-13 23:23:18 +0100

    Intitialise project

---
+++ sodokuSolver.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+class SodokuSolver {
+
+    public: SodokuSolver() {
+
+        menu();
+
+    }
+
+    private: void menu() {
+
+        // Inital display when program starts.
+
+    }
+
+
+};
